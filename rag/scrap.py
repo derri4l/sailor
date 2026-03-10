f807aed030b833cff68e3dbe96f8192447b96bc4
derri4l <dsimons0001@gmail.com>
2026-03-10 03:10:26 -0500

    added error handling

--- rag/scrap.py
+++ rag/scrap.py
@@ -6,29 +6,40 @@ KNOWLEDGE_DIR = "../knowledge/"
 
 
 def scrape(url: str):
-    response = requests.get(url)
-    html = response.text
+    try:
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        html = response.text
 
 
 # parse and extract the txt
-    soup = BeautifulSoup(html, "html.parser")
-    text = soup.get_text()
-
-# remove extra whitespaces
-    lines = [line.strip() for line in text.splitlines()]
-    clean = "\n\n".join(lines)
+        soup = BeautifulSoup(html, "html.parser")
+        text = soup.get_text()
+        lines = [line.strip() for line in text.splitlines()]
+        clean = "\n\n".join(lines)
 
 # create filename and save in Knowledge dir
-    filename = url.split("/")[-1] or "untitled"
-    filepath = os.path.join(KNOWLEDGE_DIR, f"{filename}.txt")
+        filename = url.split("/")[-1] or "untitled"
+        filepath = os.path.join(KNOWLEDGE_DIR, f"{filename}.txt")
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(clean)
 
-    with open(filepath, "w", encoding="utf-8") as f:
-        f.write(clean)
+        print(f"Saved: {filepath}")
+        print(f"Chars count: {len(clean)}")
 
-    print(f"Saved: {filepath}")
-    print(f"Chars count: {len(clean)}")
+# site returns error
+    except requests.exceptions.HTTPError as e:
+        print (f"HTTP error: {e}")
+
+#ctrl + c to exit
+    except KeyboardInterrupt:
+        print("\nExited")
 
 # read
 if __name__ == "__main__":
-    url = input("Paste URL: ")
-    scrape(url)
+    try:
+        url = input("Paste URL: ")
+        scrape(url)
+    except KeyboardInterrupt:
+        print("\nExited.")
